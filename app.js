45f446b8598d310dae323193032da9bf05505f53
Samuel Oloruntoba <tag@kayandra.co>
2019-10-14 14:11:16 +0100

    auto update user location and weather

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const fetch = require("node-fetch");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var app = express();
+const app = express();
+
+// register environment variables
+require("dotenv").config();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("trust proxy", true);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,57 +21,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", function(req, res, next) {
-  res.render("index", {
-    title: "Weather app",
-    weather: {
-      request: {
-        type: "City",
-        query: "New York, United States of America",
-        language: "en",
-        unit: "m"
-      },
-      location: {
-        name: "New York",
-        country: "United States of America",
-        region: "New York",
-        lat: "40.714",
-        lon: "-74.006",
-        timezone_id: "America/New_York",
-        localtime: "2019-09-07 08:14",
-        localtime_epoch: 1567844040,
-        utc_offset: "-4.0"
-      },
-      current: {
-        observation_time: "12:14 PM",
-        temperature: 13,
-        weather_code: 113,
-        weather_icons: [
-          "https://assets.weatherstack.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png"
-        ],
-        weather_descriptions: ["Sunny"],
-        wind_speed: 0,
-        wind_degree: 349,
-        wind_dir: "N",
-        pressure: 1010,
-        precip: 0,
-        humidity: 90,
-        cloudcover: 0,
-        feelslike: 13,
-        uv_index: 4,
-        visibility: 16
-      }
-    }
-  });
+app.use("/", async (req, res, next) => {
+  const locKey = process.env.LOCATION_KEY;
+  const locResponse = await fetch(
+    `http://api.ipstack.com/${req.ip}?access_key=${locKey}`
+  );
+  const location = await locResponse.json();
+
+  // weather request
+  const weatherKey = process.env.WEATHER_KEY;
+  const response = await fetch(
+    `http://api.weatherstack.com/current?access_key=${weatherKey}&query=${location.city}`
+  );
+  const weather = await response.json();
+
+  // respond
+  res.render("index", { title: "Weather app", weather });
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
